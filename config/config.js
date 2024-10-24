a73e41642dfd60157b693504e3a603342e6672cb
arianakhbari <77466277+arianakhbari@users.noreply.github.com>
2024-10-24 13:50:38 -0700

    config.js

--- config/config.js
+++ config/config.js
@@ -1,9 +1,12 @@
 // config/config.js
+
 require('dotenv').config();
 
 module.exports = {
-    BOT_TOKEN: process.env.BOT_TOKEN, // مقدار توکن از فایل .env خوانده می‌شود
-    ADMIN_IDS: process.env.ADMIN_IDS ? process.env.ADMIN_IDS.split(',').map(id => parseInt(id.trim())) : [],
+    BOT_TOKEN: process.env.BOT_TOKEN,
+    ADMIN_IDS: process.env.ADMIN_IDS
+        ? process.env.ADMIN_IDS.split(',').map(id => parseInt(id.trim()))
+        : [],
     DB: {
         dialect: 'sqlite',
         storage: 'bot.db',
